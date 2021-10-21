9bb9b81bc27a16fc6f36560096d2723744ba0adf
DoGooRi <gudjsgh6@gmail.com>
2021-10-21 20:25:19 +0900

    수급자, 예비수급자 리스트 마스킹 및 복호화 추가 (이름, 요양인정번호, 연락처, 보호자명, 보호자연락처)

--- src/main/java/kr/co/thkc/mybatis/CamelListMap.java
+++ src/main/java/kr/co/thkc/mybatis/CamelListMap.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import org.apache.commons.collections4.map.ListOrderedMap;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -19,6 +20,11 @@ public class CamelListMap extends ListOrderedMap<Object, Object> {
      */
     private final String decryptCode = "SDB_";
     private static final long serialVersionUID = 1L;
+
+    // 수급자 정보 마스킹
+    private final String[] NAME = {"SDB_PEN_NM", "SDB_PEN_PRO_NM"};
+    private final String[] LTM_NUM = {"SDB_PEN_LTM_NUM"};
+    private final String[] PHONE = {"SDB_PEN_CON_NUM", "SDB_PEN_CON_PNUM", "SDB_PEN_PRO_CON_NUM", "SDB_PEN_PRO_CON_PNUM"};
 
     private String toProperCase(String s, boolean isCapital) {
 
@@ -51,8 +57,24 @@ public class CamelListMap extends ListOrderedMap<Object, Object> {
             try {
                 SDBCryptUtil sdb = new SDBCryptUtil();
                 value = sdb.decrypt(value.toString());
+
+                // 이름
+                if (Arrays.asList(NAME).contains(key.toString()) && value.toString().length() > 1) {
+                    value = nameMasking(value.toString());
+                }
+                // 핸드폰
+                if (Arrays.asList(LTM_NUM).contains(key.toString()) && value.toString().length() > 5) {
+                    value = ltmNumMasking(value.toString());
+                }
+                // 연락처
+                if (Arrays.asList(PHONE).contains(key.toString()) && value.toString().length() > 5) {
+                    value = phoneMasking(formatPhone(value.toString()));
+                }
+
+                // 키 원래대로 수정
                 key = key.toString().replaceFirst(decryptCode, "");
                 sdb = null;
+
             } catch (Exception e) {
                 throw new Exception(value + " can't decrypt");
             }
@@ -63,4 +85,55 @@ public class CamelListMap extends ListOrderedMap<Object, Object> {
         return super.put(toCamelCase((String) key), (value == null || value.equals(" ") ? null : value));
     }
 
+    public static String nameMasking(String name) throws Exception {
+        String result;
+
+        if (name.length() <= 2) {
+            result = name.substring(0, 1) + new String(new char[name.length() - 1]).replace("\0", "*");;
+        } else {
+            result = name.substring(0, 2) + new String(new char[name.length() - 2]).replace("\0", "*");;
+        }
+
+        return result;
+    }
+
+    public static String ltmNumMasking(String num) throws Exception {
+        return num.substring(0, 6) + "*****";
+    }
+
+    public static String phoneMasking(String phoneNo) throws Exception {
+//        String regex = "(\\d{2,3})-?(\\d{3,4})-?(\\d{4})$";
+//        Matcher matcher = Pattern.compile(regex).matcher(phoneNo);
+//        if (matcher.find()) {
+//            String target = matcher.group(2);
+//            int length = target.length();
+//            char[] c = new char[length];
+//            Arrays.fill(c, '*');
+//            return phoneNo.replace(target, String.valueOf(c));
+//        }
+
+        String head = phoneNo.substring(0, 5);
+        String tail = phoneNo.substring(phoneNo.length() - 1, phoneNo.length());
+        String maskedPhoneNo;
+
+        if (phoneNo.length() == 13) {
+            maskedPhoneNo = head + "***-***" + tail;
+        } else {
+            maskedPhoneNo = head + "**-***" + tail;
+        }
+
+        return maskedPhoneNo;
+    }
+
+    public static String formatPhone(String src) {
+        if (src == null) {
+            return "";
+        }
+        if (src.length() == 8) {
+            return src.replaceFirst("^([0-9]{4})([0-9]{4})$", "$1-$2");
+        } else if (src.length() == 12) {
+            return src.replaceFirst("(^[0-9]{4})([0-9]{4})([0-9]{4})$", "$1-$2-$3");
+        }
+        return src.replaceFirst("(^02|[0-9]{3})([0-9]{3,4})([0-9]{4})$", "$1-$2-$3");
+    }
 }
